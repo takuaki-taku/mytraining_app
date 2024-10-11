a6f66aef62132973774b08b04b596d6bb5d0ef26
takuaki <tatennisku@gmail.com>
2024-10-11 12:44:23 +0900

    Add Docker integration

--- run.py
+++ run.py
@@ -10,7 +10,8 @@ def make_shell_context():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8001)
+    # アプリケーションが外部からアクセス可能になるように、hostを0.0.0.0に設定
+    app.run(debug=True, host="0.0.0.0", port=5000)
 
 # アプリケーションの起動時
 with app.app_context():
